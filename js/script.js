4749ec10daadefe6c9e0d370f418b97ecfd1dd82
heather_l <heatherlangrall@gmail.com>
2023-09-29 15:23:00 -0400

    JS fixes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Your JS code here
 var subTypeElement = document.querySelector("#subscription");
 var subDurationElement = document.querySelector("#months");
-var result = document.querySelector("#result");
+var result = document.querySelector(".result");
 var subType = "basic"
 var subDuration = 1;
 
